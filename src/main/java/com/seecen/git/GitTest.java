d5747c6caa328f6d1d353014ab602b986d8cbf08
chenyue <291975311@qq.com>
2022-07-21 22:24:42 +0800

    first commit

---
+++ src/main/java/com/seecen/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.seecen.git;
+
+/**
+ * @author cystart
+ * @date 2022/7/21 - 22:18
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
