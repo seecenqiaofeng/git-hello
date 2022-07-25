1b789233e3461c6703cee8e4bf2b5951aca7eea8
chenyue <291975311@qq.com>
2022-07-25 22:16:14 +0800

    push commit2

--- src/main/java/com/seecen/git/GitTest.java
+++ src/main/java/com/seecen/git/GitTest.java
@@ -13,5 +13,7 @@ public class GitTest {
         System.out.println("hello git-hotfix");
         System.out.println("master text");
         System.out.println("hotfix test");
+
+        System.out.println("push test");
     }
 }
