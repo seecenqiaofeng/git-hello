9fcce5a6125cc23895d505bfcf1f1a5c506831e7
chenyue <291975311@qq.com>
2022-07-25 13:53:57 +0800

    master commit2

--- src/main/java/com/seecen/git/GitTest.java
+++ src/main/java/com/seecen/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git-hotfix");
+        System.out.println("master text");
     }
 }
